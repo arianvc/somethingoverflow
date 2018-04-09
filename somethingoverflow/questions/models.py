0a9199f8001579453816897c12dee09064a58a9b
arian <arianvc@gmail.com>
2018-04-09 13:13:10 +0430

    fixed admin and model str representations

--- somethingoverflow/questions/models.py
+++ somethingoverflow/questions/models.py
@@ -12,21 +12,21 @@ dict2tuple4sqlenum = lambda da_dict: tuple((k, da_dict[k]) for k in da_dict)
 
 class Question(models.Model):
     def __str__(self):
-        return '{} by {} from {}'.format(self.id, self.title, self.author)
+        return '{} - {} by {}'.format(self.id, self.title, self.author)
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, related_name='questions_by')
 
 
 class Post(models.Model):
     def __str__(self):
-        return '{} by {} from {}'.format(self.id, self.title, self.author)
+        return '{} Post {}... for '.format(self.id, self.body[:10], self.question.title)
     body = models.TextField()
     question = models.ForeignKey(Question, related_name='questions_by')
 
 
 class Reaction(models.Model):  # TODO: not efficient, what's the alternative
     def __str__(self):
-        return '{} by {} from {}'.format(self.id, self.title, self.author)
+        return '{}-{}-{} reaction {}'.format(self.id, self.author, self.post, self.author)
     TYPE = {'p':'Plus', 'm':'Minus'}
     status = models.CharField(max_length=1, default='p', choices=dict2tuple4sqlenum(TYPE))
     post = models.ForeignKey(Post, related_name='reactions_for')
