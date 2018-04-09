4457a4582a994982ca0bca8681d5207e04c948c2
arian <arianvc@gmail.com>
2018-04-09 15:20:10 +0430

    forms added, views defined

---
+++ somethingoverflow/questions/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Question, Post
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'})
+        }
+
+
+class RecoverForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    # TODO: captcha
+    # TODO: SMS recovery
+
+
+class ResetForm(forms.Form):  # TODO: use modelform for use with different fields?
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    recovery_code = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Recovery Code'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    new_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password, 8 alphanumeric at least'}))
+
+
+class Question(forms.ModelForm):
+    class Meta:
+        model = Question
+
+
+class Post(forms.ModelForm):
+    class Meta:
+        model = Post
--- somethingoverflow/questions/views.py
+++ somethingoverflow/questions/views.py
@@ -1,10 +1,60 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import string
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.models import User, Group
+from .models import Question, Post, Reaction
+from .forms import LoginForm, RegisterForm, RecoverForm, NewPostForm, AnswerForm
+
+# Handy functions
+random_str = lambda N: ''.join(
+    random.SystemRandom().choice(
+        string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(N))
 
 
 # Create your views here.
 
-def log_in(request):
+def log_in(request, **kwargs):
+    context = { 'message':kwargs['message'] if 'message' in kwargs else 'Welcome!',
+                'loginform': LoginForm,
+                'registerform': RegisterForm,
+                'recoverform': RecoverForm,
+                }
+    if request.user.is_authenticated:
+        return redirect('questions')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('tasks')#TODO, message=context['message'])
+                else:
+                    context['message'] = 'Disabled account'
+            else:
+                context['message'] = 'Invalid login'
+        else:
+            form = LoginForm()
+    return render(request, 'login.html', context)
+
+
+def questions(request):
+    pass
+
+
+def ask(request):
+    pass
+
+
+def answer(request):
+    pass
+
+
+def react(request):
     pass
